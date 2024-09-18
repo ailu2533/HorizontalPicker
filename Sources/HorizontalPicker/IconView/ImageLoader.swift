22d499aed1d12799dce8342ab04d4ea51dcad0dd
ailu <im.ailu@outlook.com>
2024-09-18 16:44:58 +0800

    update

--- Sources/HorizontalPicker/IconView/ImageLoader.swift
+++ Sources/HorizontalPicker/IconView/ImageLoader.swift
@@ -1,6 +1,6 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by ailu on 2024/7/12.
 //
@@ -8,21 +8,20 @@
 import Foundation
 import UIKit
 
-
 class ImageLoader: ObservableObject {
     @Published var image: UIImage?
-    @Published var isLoading: Bool = false  // 添加一个标记图像是否正在加载的状态
-    static let sharedCache = NSCache<NSString, UIImage>()  // 全局静态缓存
+    @Published var isLoading: Bool = false // 添加一个标记图像是否正在加载的状态
+    static let sharedCache = NSCache<NSString, UIImage>() // 全局静态缓存
 
     func loadAndResizeImage(named imageName: String, targetSize: CGSize) {
-        isLoading = true  // 标记开始加载
+        isLoading = true // 标记开始加载
         let cacheKey = NSString(string: "\(imageName)_\(targetSize.width)x\(targetSize.height)")
-        
+
         // 尝试从全局缓存中获取图像
         if let cachedImage = ImageLoader.sharedCache.object(forKey: cacheKey) {
             DispatchQueue.main.async {
                 self.image = cachedImage
-                self.isLoading = false  // 标记加载完成
+                self.isLoading = false // 标记加载完成
             }
             return
         }
@@ -31,21 +30,21 @@ class ImageLoader: ObservableObject {
         DispatchQueue.global(qos: .userInitiated).async {
             guard let image = UIImage(named: imageName) else {
                 DispatchQueue.main.async {
-                    self.image = UIImage()  // 使用空图片作为默认值
-                    self.isLoading = false  // 标记加载完成
+                    self.image = UIImage() // 使用空图片作为默认值
+                    self.isLoading = false // 标记加载完成
                 }
                 return
             }
             let format = UIGraphicsImageRendererFormat()
-            format.scale = UIScreen.main.scale  // 使用设备的屏幕比例
+            format.scale = UIScreen.main.scale // 使用设备的屏幕比例
             let renderer = UIGraphicsImageRenderer(size: targetSize, format: format)
             let newImage = renderer.image { _ in
                 image.draw(in: CGRect(origin: .zero, size: targetSize))
             }
             DispatchQueue.main.async {
                 self.image = newImage
-                ImageLoader.sharedCache.setObject(newImage, forKey: cacheKey)  // 将新图像存入全局缓存
-                self.isLoading = false  // 标记加载完成
+                ImageLoader.sharedCache.setObject(newImage, forKey: cacheKey) // 将新图像存入全局缓存
+                self.isLoading = false // 标记加载完成
             }
         }
     }
--- Sources/HorizontalPicker/IconView/ResizedImage.swift
+++ Sources/HorizontalPicker/IconView/ResizedImage.swift
@@ -1,18 +1,23 @@
 //
 //  SwiftUIView.swift
-//  
+//
 //
 //  Created by ailu on 2024/7/12.
 //
 
 import SwiftUI
 
-struct ResizedImage: View {
+public struct ResizedImage: View {
     @StateObject var imageLoader = ImageLoader()
     let imageName: String
     let targetSize: CGSize
 
-    var body: some View {
+    public init(imageName: String, targetSize: CGSize) {
+        self.imageName = imageName
+        self.targetSize = targetSize
+    }
+
+    public var body: some View {
         Group {
             if let image = imageLoader.image {
                 Image(uiImage: image)
@@ -21,11 +26,11 @@ struct ResizedImage: View {
                     .transition(.opacity)
                     .animation(.easeInOut(duration: 0.5), value: imageLoader.image)
             } else if imageLoader.isLoading {
-                ProgressView()  // 显示加载进度指示器
+                ProgressView() // 显示加载进度指示器
                     .aspectRatio(contentMode: .fit)
             } else {
                 Rectangle()
-                    .fill(Color.clear)// 提供一个占位符
+                    .fill(Color.clear) // 提供一个占位符
             }
         }
         .onAppear {
